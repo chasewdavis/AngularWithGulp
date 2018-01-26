9196a54d37ed4b30eb94cc7400dc465a117fe9f3
Chase Davis <chasewdavis7@gmail.com>
2018-01-25 16:22:30 -0800

    mobile slider working smoothly

--- src/js/controllers/demoCtrl.js
+++ src/js/controllers/demoCtrl.js
@@ -1,16 +1,17 @@
-angular.module('App').controller('demoCtrl', ['$scope', 'srvc', function($scope, srvc){
+angular.module('App').controller('demoCtrl', ['$scope', 'srvc', '$timeout', function($scope, srvc, $timeout){
 
     $scope.questions = srvc.getFinishedQuestions();
 
-    $scope.currentIndex = 9;
+    $scope.currentIndex = 4;
 
     var pages = document.getElementsByClassName('pages')[0];
     pages.addEventListener('touchstart', handleStart, false)
     pages.addEventListener('touchmove', handleMove, false)
     pages.addEventListener('touchend', handleStop, false)
 
+    var singlePageWidth = pages.offsetWidth / 5;
     var initialOffset = pages.offsetWidth * 2 / 5;
-    var thresholdX = pages.offsetWidth * 1 / 15;
+    var thresholdX = singlePageWidth / 3;
     var initialX;
     var currentX;
     var changeInX;
@@ -30,28 +31,61 @@ angular.module('App').controller('demoCtrl', ['$scope', 'srvc', function($scope,
 
     function handleStop(){
         totalDistance += currentX-initialX;
-        // move to next slide if swipe is long enough
+        // move to next or previous slide according to swipe gesture
         if( changeInX > thresholdX ){
-            console.log('move right')
-        }else if ( changeInX < -thresholdX ){
-            console.log('move left')
+            pages.style.transition = 'all .3s';
+            pages.style.transform = `translate(${-initialOffset + singlePageWidth }px)`;
+            $timeout(function(){
+                pages.style.transition = 'none';
+                totalDistance = 0;
+                $scope.currentIndex ? $scope.currentIndex-- : $scope.currentIndex = 9;
+                displayCards();
+                pages.style.transform = `translate(${-initialOffset}px)`;
+            },300);
+        } else if ( changeInX < -thresholdX ){
+            pages.style.transition = 'all .3s';
+            pages.style.transform = `translate(${-initialOffset - singlePageWidth }px)`;
+            $timeout(function(){
+                pages.style.transition = 'none';
+                totalDistance = 0;
+                $scope.currentIndex === 10 ? $scope.currentIndex = 0 : $scope.currentIndex++;
+                displayCards();
+                pages.style.transform = `translate(${-initialOffset}px)`;
+            },300);
+        } else {
+            pages.style.transition = 'all .3s';
+            pages.style.transform = `translate(${-initialOffset}px)`;
+            $timeout(function(){
+                pages.style.transition = 'none';
+                totalDistance = 0;
+            },300);
         }
     }
 
     //show two cards before current card, and two cards after current card
     $scope.cards;
 
-    if($scope.currentIndex > 1 && $scope.currentIndex < 8) {
-        $scope.cards = $scope.questions.slice($scope.currentIndex-2,$scope.currentIndex+3)
-    } else if ($scope.currentIndex <= 1 ) {
-        $scope.cards = $scope.questions.slice(0, $scope.currentIndex + 3)
-        var remaining = 5 - $scope.cards.length;
-        $scope.cards.unshift( ...$scope.questions.slice( $scope.questions.length - remaining ) )
-    } else if ($scope.currentIndex >= 8) {
-        $scope.cards = $scope.questions.slice( $scope.currentIndex - 2 );
-        var remaining = 5 - $scope.cards.length;
-        $scope.cards.push( ...$scope.questions.slice(0, remaining ) )
+    function displayCards(){
+
+        console.log($scope.currentIndex)
+
+        if($scope.currentIndex > 1 && $scope.currentIndex < 8) {
+            $scope.cards = $scope.questions.slice($scope.currentIndex-2,$scope.currentIndex+3)
+        } else if ($scope.currentIndex <= 1 ) {
+            $scope.cards = $scope.questions.slice(0, $scope.currentIndex + 3)
+            var remaining = 5 - $scope.cards.length;
+            $scope.cards.unshift( ...$scope.questions.slice( $scope.questions.length - remaining ) )
+        } else if ($scope.currentIndex >= 8) {
+            $scope.cards = $scope.questions.slice( $scope.currentIndex - 2 );
+            var remaining = 5 - $scope.cards.length;
+            $scope.cards.push( ...$scope.questions.slice(0, remaining ) )
+        }
+
+        console.log($scope.cards)
+
     }
+
+    displayCards();
     
 
 }])
